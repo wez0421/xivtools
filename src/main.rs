6953a96cc04a8ee47372507f767f058314113ac8
Christopher Anderson <cja@google.com>
2018-09-18 14:15:09 -0700

    [rust] Add macro parsing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
-use std::collections::HashMap;
-use std::env;
-extern crate config;
+//use std::collections::HashMap;
+mod recipe;
+//use config;
+#[macro_use]
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-        println!("invalid argument count!");
-        println!("usage: {} [path/to/settings/file] [path to macro file] [craft count]", args[0]);
-        return;
-    }
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Item(s) being crafted are collectible
+    #[structopt(short = "c", long = "collectible")]
+    collectible: bool,
+    /// Print verbose information during execution
+    #[structopt(short = "v", long = "verbose")]
+    verbose: bool,
+    /// Number of items to craft
+    #[structopt(short = "n", long = "count", default_value = "1")]
+    count: u32,
+    /// Path to the file containing the XIV macro to use
+    #[structopt(name = "macro file", parse(from_os_str))]
+    macro_file: PathBuf,
+}
 
-    let settings_file = &args[1];
-    let _macro_file = &args[2];
-    let _run_count = &args[3];
-    let mut settings = config::Config::default();
-    settings.merge(config::File::with_name(settings_file)).expect("Failed to read config");
+fn main() -> Result<(), String> {
+    let opt = Opt::from_args();
 
-    println!("{:?}\n",
-             settings.try_into::<HashMap<String, String>>().unwrap());
+    println!("{:?}", opt);
 
-
-    println!("{:?}", args);
+    //let _recipe = recipe::Recipe {};
+    //let settings_file = &args[1];
+    //let _macro_file = &args[2];
+    //let _run_count = &args[3];
+    //let mut settings = config::Config::default();
+    //settings.merge(config::File::with_name(settings_file))?;
+    let (foo, bar) = recipe::parse_macro_line(r#"/ac Innovation <wait.2>""#).unwrap();
+    println!("{} {}", foo, bar);
+    return Ok(());
 }
