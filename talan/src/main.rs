9991d38a816ac609d2ef87cac43689cacccbb1e9
Christopher Anderson <chris@nullcode.org>
2019-07-15 23:48:06 -0700

    [talan] Gate gui by the -g flag

--- talan/src/main.rs
+++ talan/src/main.rs
@@ -3,25 +3,30 @@ mod gui;
 mod macros;
 mod task;
 
+use clap::{App, Arg};
 use env_logger;
 use failure::Error;
 
-//mod craft;
-//mod role_actions;
-//mod recipe;
+const VERSION: &str = "1.0";
 
 fn main() -> Result<(), Error> {
     env_logger::init();
 
-    // // Grab and parse the config file. Errors are all especially fatal so
-    // // let them bubble up if they occur.
-    // let macro_contents =
-    //     macros::parse_file(opt.macro_file).map_err(|e| format!("error parsing macro: `{}`", e));
+    let matches = App::new("Ventures")
+        .version(VERSION)
+        .arg(
+            Arg::with_name("gui")
+                .short("g")
+                .help("Run with a graphical interface."),
+        )
+        .get_matches();
 
     let _handle = xiv::init()?;
     let mut cfg = config::read_config();
     let macros = macros::get_macro_list()?;
-    gui::start(&mut cfg, &macros)?;
-    //craft::craft_items(&handle, &tasks);
+
+    if matches.occurrences_of("gui") > 0 {
+        gui::start(&mut cfg, &macros)?;
+    }
     Ok(())
 }
